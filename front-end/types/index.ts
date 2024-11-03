0d626d0f918f278abd23583bceacf2b271c17a51
Rostislav-Dusnii <rostislav.dusnii@student.ucll.be>
2024-11-03 21:57:53 +0000

    refactor

--- front-end/types/index.ts
+++ front-end/types/index.ts
@@ -15,3 +15,26 @@ export type CourseShort = {
   phase: number;
   credits: number;
 };
+
+export type CourseUpdateView = {
+  name: string;
+  description: string;
+  phase: number;
+  credits: number;
+  lecturers: string[];
+  isElective: boolean;
+  requiredPassedCourses: number[];
+};
+
+
+export function convertCourseToUpdateView(course: Course): CourseUpdateView {
+  return {
+    name: course.name,
+    description: course.description,
+    phase: course.phase,
+    credits: course.credits,
+    lecturers: course.lecturers,
+    isElective: course.isElective,
+    requiredPassedCourses: course.requiredPassedCourses.map(c => c.id),
+  };
+}
