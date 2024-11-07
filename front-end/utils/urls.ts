cf0110b2c3294807a10bfb193c8a302b7e62fb81
Rostislav-Dusnii <rostislav.dusnii@student.ucll.be>
2024-11-07 12:10:02 +0000

    F2 added urls

--- front-end/utils/urls.ts
+++ front-end/utils/urls.ts
@@ -1,3 +1,8 @@
 export const HOME_URL = '/';
 export const LOGIN_URL = '/login';
+export const COMPOSE_ISP_URL = '/compose-isp';
+export const VIEW_ISP_URL = '/view-isp';
+export const PAYMENTS_URL = '/payments';
+export const MANAGE_PROFILES_URL = '/manage-profiles';
+export const MANAGE_COURSES_URL = '/manage-courses';
 export const BACKEND_APP_URL='http://localhost:3000'
