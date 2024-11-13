7352843f3f9782d2bac22997b9d9ee17def18d98
Rostislav-Dusnii <rostislav.dusnii@student.ucll.be>
2024-11-13 09:11:08 +0000

    F5 changed urls and tabs

--- front-end/types/tab.ts
+++ front-end/types/tab.ts
@@ -1,4 +1,4 @@
-import { COMPOSE_ISP_URL, VIEW_ISP_URL, PAYMENTS_URL, MANAGE_COURSES_URL, MANAGE_PROFILES_URL } from "@/utils/urls";
+import { MANAGE_ISP_URL, PAYMENTS_URL, MANAGE_COURSES_URL, MANAGE_PROFILES_URL } from "@/utils/urls";
 
 export type Tab = {
     name: string;
@@ -6,8 +6,7 @@ export type Tab = {
 };
 
 export const tabs: Tab[] = [
-    { name: "Compose ISP", href: COMPOSE_ISP_URL },
-    { name: "View my ISP", href: VIEW_ISP_URL },
+    { name: "Manage ISP", href: MANAGE_ISP_URL },
     { name: "Payments", href: PAYMENTS_URL },
     { name: "Manage Profiles", href: MANAGE_PROFILES_URL },
     { name: "Manage Courses", href: MANAGE_COURSES_URL },
--- front-end/utils/urls.ts
+++ front-end/utils/urls.ts
@@ -1,7 +1,6 @@
 export const HOME_URL = '/';
 export const LOGIN_URL = '/login';
-export const COMPOSE_ISP_URL = '/compose-isp';
-export const VIEW_ISP_URL = '/view-isp';
+export const MANAGE_ISP_URL = '/manage-isp';
 export const PAYMENTS_URL = '/payments';
 export const MANAGE_PROFILES_URL = '/manage-profiles';
 export const MANAGE_COURSES_URL = '/manage-courses';
