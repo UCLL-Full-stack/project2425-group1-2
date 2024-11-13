1ac45e80ccecee3c97a04b0eb0c6a63bec8f89a4
Rostislav-Dusnii <rostislav.dusnii@student.ucll.be>
2024-11-13 20:39:13 +0000

    F6 added proper file structure

--- front-end/types/tab.ts
+++ front-end/types/tab.ts
@@ -1,4 +1,4 @@
-import { MANAGE_ISP_URL, PAYMENTS_URL, MANAGE_COURSES_URL, MANAGE_PROFILES_URL } from "@/utils/urls";
+import { MANAGE_ISP_URL, PAYMENTS_URL, MANAGE_COURSES_URL, MANAGE_PROFILES_URL, MY_ISP_URL } from "@/utils/urls";
 
 export type Tab = {
     name: string;
@@ -6,8 +6,9 @@ export type Tab = {
 };
 
 export const tabs: Tab[] = [
-    { name: "Manage ISP", href: MANAGE_ISP_URL },
+    { name: "My ISP", href: MY_ISP_URL },
     { name: "Payments", href: PAYMENTS_URL },
+    { name: "Manage ISP", href: MANAGE_ISP_URL },
     { name: "Manage Profiles", href: MANAGE_PROFILES_URL },
     { name: "Manage Courses", href: MANAGE_COURSES_URL },
 ];
--- front-end/utils/urls.ts
+++ front-end/utils/urls.ts
@@ -4,4 +4,5 @@ export const MANAGE_ISP_URL = '/manage-isp';
 export const PAYMENTS_URL = '/payments';
 export const MANAGE_PROFILES_URL = '/manage-profiles';
 export const MANAGE_COURSES_URL = '/manage-courses';
+export const MY_ISP_URL = '/my-isp';
 export const BACKEND_APP_URL='http://localhost:3000'
