34a0a80330f683ecb5610c3bca06e08ebcbabf1c
Rostislav-Dusnii <rostislav.dusnii@student.ucll.be>
2024-12-19 15:37:23 +0100

    B6: added get student by id

--- back-end/repository/student.db.ts
+++ back-end/repository/student.db.ts
@@ -1,13 +1,34 @@
-import {Student} from '../model/student';
-import students from '../data/students';
+import prismaClient from './prisma/prismaClient'
 import tryCatcher from '../util/tryCatchWrapper';
+import { Student } from '../model/student';
 
-let DBstudents: Student[] = students;
-
-const findAllByPassedCourseId = tryCatcher((id: number): Student[] => {
-    return DBstudents.filter(student => student.passedCourses.some(course => course.id === id));
+const findById = tryCatcher(async (id: number): Promise<Student> => {
+    const student = await prismaClient.user.findUnique({
+        where: { id }
+    });
+    if (!student) {
+        throw new Error(STUDENT_NOT_FOUND_ERROR);
+    }
+    return Student.from(student);
 });
 
+const findAllByPassedCourseId = tryCatcher(async (courseId: number): Promise<Student[]> => {
+    const students = await prismaClient.user.findMany({
+        where: {
+            userType: 'STUDENT',
+            courses: {
+                some: {
+                    courseId: courseId
+                }
+            }
+        }
+    });
+    return students.map(Student.from);
+});
+
+const STUDENT_NOT_FOUND_ERROR = 'Student not found';
+
 export default {
+    findById,
     findAllByPassedCourseId,
 };
